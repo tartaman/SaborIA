064400025e03f7590ef6234c3976306dd6e43cc0
Tartamannnn <popolol5342@gmail.com>
2024-09-27 17:15:38 -0600

    xd

---
+++ scripts/preventSQLi.js
@@ -0,0 +1,7 @@
+// Seleccionamos todos los inputs y les agregamos un event listener
+document.querySelectorAll("input").forEach((input) => {
+    input.addEventListener('input', function() {
+        // Permitir letras, números, espacios y el punto (.)
+        this.value = this.value.replace(/[^a-zA-Z0-9 .@]/g, '');
+    });
+});
