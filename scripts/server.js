bfe4dae41e845a51652da936332f5e9f2e617419
Tartamannnn <popolol5342@gmail.com>
2024-09-30 23:44:08 -0600

    Codigo repetido
    upsi

--- scripts/server.js
+++ scripts/server.js
@@ -117,11 +117,6 @@ app.post('/upload', authMiddleware, upload.single('image'), (req, res) => {
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 //Relacionado a verificacion o pulls a la db ------------------------------------------------------------------
-//Endpoint para verificar el token
-app.post('/verify-token', authMiddleware, (req, res) => {
-  // Si llega hasta aquí, significa que el token es válido
-  res.status(200).json({ valid: true });
-});
 //aqui se crean las API basicamente todo lo que tenga get, va a hacer una api de la cual podremos descargar datos
 app.get("/dificultades", (req, res) => {
   connection.query("SELECT id_dificultad, nombre FROM dificultad;", (err, result) => {
@@ -185,91 +180,6 @@ app.post('/agregar-receta', authMiddleware, (req, res) => {
         res.status(201).json({ message: 'Receta agregada correctamente', recetaId: result.insertId });
     });
 });
-
-app.post("/addEmail", (req,res) => {
-  let uploadedToDatabase = false;
-  const {nombre, apat, amat,correo,username,pass} = req.body
-  if (!nombre || !apat || !correo || !username || !pass) 
-    return res.status(400).json({message: "Campo o Campos obligatorios no llenados"});
-  console.log(`voy a subir ${(nombre, apat, amat, correo, username,pass)}`)
-  const token = crypto.randomBytes(32).toString('hex');
-  // Hasheamos la contraseña
-  bcrypt.genSalt(saltRounds, (err, salt) => {
-    if (err) throw err;
-
-    bcrypt.hash(pass, salt, (err, hash) => {
-      if (err) throw err;
-      
-      const query = "INSERT INTO usuario (nombre, apellido_paterno, apellido_materno, correo, pass, estado, token, username) VALUES(?, ?, ?, ?, ?, ?, ?, ?);"
-      connection.query(query, [nombre, apat,amat, correo,hash,0,token, username], (err,result) => {
-        if (err) {
-          console.log(err)
-          return res.status(500).json({message:"Error al registrar usuario"});
-        }
-        uploadedToDatabase = true;
-
-        res.status(201).json({message: "Registrado correctamente", id: result.insertId});
-      })
-      uploadedToDatabase = true;
-      if (uploadedToDatabase) {
-        const mailOptions = {
-          from: `${correoSaboria}`,
-          to: `${correo}`,
-          subject: 'Verifica tu cuenta en SaborIA',
-          text: `Por favor verifica tu correo entrando a este enlace: ${process.env.APIHOST}/verify-email?token=${token}`,
-        };
-
-        transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-            console.error('Error al enviar el correo:', error);
-          } else {
-            console.log('Correo enviado: ' + info.response);
-          }
-        });
-      }
-    }
-  )});
-});
-app.post("/login", (req, res) => {
-  const { username, pass } = req.body;
-  
-  connection.query("SELECT * FROM usuario WHERE username = ?", [username], (err, result) => {
-    if (err) {
-      console.log(err);
-      return res.status(500).json({ message: "Error en el servidor" });
-    }
-
-    if (result.length === 0) {
-      // Si el usuario no existe
-      return res.status(401).json({ message: "Usuario no encontrado" });
-    }
-    if (result) {
-      //Si devuelve algo, imprimelo
-      console.log(result)
-    }
-    if (result[0].estado == 0) {
-      return res.status(401).json({message: "Por favor, verifica tu correo antes de acceder"})
-    }
-    const storedHash = result[0].pass;
-    bcrypt.compare(pass, storedHash, function(err, isMatch) {
-      if (err) throw err;
-
-      if (isMatch) { // Si la contraseña es correcta
-        const token = jwt.sign({ userId: result[0].id_usuario, email: result[0].correo }, process.env.JWTHASH, { expiresIn: '10h' });
-        return res.status(200).json({ token, user: { userid: result[0].id_usuario, email: result[0].correo } });
-      } else { // Si la contraseña es incorrecta
-        return res.status(401).json({ message: "Contraseña incorrecta" });
-      }
-    });
-  });
-});
-
-
-//no me maten basicamente querys cada 5 segundos para que el servidor no me saque
-setInterval(() => {
-  connection.query('SELECT 1');
-}, 5000); 
-
 //Relacionado a verificacion o pulls a la db ------------------------------------------------------------------
 //Endpoint para verificar el token
 app.post('/verify-token', authMiddleware, (req, res) => {
@@ -319,21 +229,6 @@ app.get('/verify-email', (req, res) => {
       }
     }
   );
-});
-
-app.post('/agregar-receta', authMiddleware, (req, res) => {
-    const { nombre, tiempo, dificultad, pasos, porciones } = req.body;
-    const userId = req.user.userId;
-
-    const query = "INSERT INTO receta (nombre, tiempo, dificultad, pasos, porciones, id_usuario) VALUES (?, ?, ?, ?, ?, ?)";
-    connection.query(query, [nombre, tiempo, dificultad, pasos, porciones, userId], (err, result) => {
-        if (err) {
-            return res.status(500).json({ message: 'Error al agregar la receta' });
-        }
-
-        // Devolver el ID de la receta recién agregada
-        res.status(201).json({ message: 'Receta agregada correctamente', recetaId: result.insertId });
-    });
 });
 
 app.post("/addEmail", (req,res) => {
