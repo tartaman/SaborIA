903682b56962c6557e7c42d69d40452dc02cd163
Tartamannnn <popolol5342@gmail.com>
2024-10-17 12:16:09 -0600

    Ya baja las recetas
    y las muestra en receta.html

---
+++ scripts/getRecetas.js
@@ -0,0 +1,27 @@
+const containerRecetas = document.querySelector('#container-recetas')
+fetch(`${API_URL}/Recetas`, {
+    method: `GET`,
+    headers: {
+        'Authorization': `Bearer ${token}` // Indicamos que estamos enviando JSON
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log("Recetas obtenidas:", data);
+
+    data.forEach(receta => {
+        mainDiv = document.createElement("div");
+        imagen = document.createElement("img");
+        titulo = document.createElement("h1");
+        titulo.textContent = receta.titulo;
+        imagen.src = `${API_URL}/uploads/${receta.codigo_imagen}`
+        imagen.alt = receta.titulo;
+        mainDiv.appendChild(titulo);
+        mainDiv.appendChild(imagen);
+        
+        containerRecetas.appendChild(mainDiv);
+    });
+})
+.catch(error => {
+    console.error("Hubo un problema al obtener las recetas:", error);
+})
