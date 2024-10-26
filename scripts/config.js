93ec29f1e124a424dc7de7e345ae73a6b0cf3bd5
Tartamannnn <popolol5342@gmail.com>
2024-10-25 20:44:51 -0600

    Dejalo asi

--- scripts/config.js
+++ scripts/config.js
@@ -1,5 +1,1 @@
-if (process.env.NODE_ENV === 'production') {
-    const API_URL = "http://44.222.84.118:3000";
-} else {
-    const API_URL = "http://localhost:3000";
-}
+const API_URL = "http://44.222.84.118:3000";
