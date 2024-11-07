e37b6088fd822568955339f506fdb9b5b79baba5
Tartamannnn <popolol5342@gmail.com>
2024-11-07 16:12:37 -0600

    Ya se pueden arrastrar los ingredientes

--- scripts/inventario.js
+++ scripts/inventario.js
@@ -1,3 +1,22 @@
+// Permite el evento de arrastre en el contenedor
+function allowDrop(event) {
+    event.preventDefault();
+}
+
+// Establece el ID del elemento arrastrado en el evento de arrastre
+function drag(event) {
+    event.dataTransfer.setData("text", event.target.id);
+}
+function drop(event) {
+    event.preventDefault();
+    const data = event.dataTransfer.getData("text");  // Obtiene el ID del elemento arrastrado
+    const draggedElement = document.getElementById(data);
+
+    // Verifica si el contenedor no tiene ya el elemento para evitar duplicados
+    if (!event.target.contains(draggedElement) && event.target == document.querySelector('#contenedor-ingredientes')) {
+        event.target.appendChild(draggedElement);  // Mueve el elemento al nuevo contenedor
+    }
+}
 async function getIngredientsinventory () {
     gottenIds = new Set();
     try {
@@ -8,7 +27,6 @@ async function getIngredientsinventory () {
             }});
         const ingredientesData = await ingredientesResponse.json()
         console.log(ingredientesData)
-        botonguardar = document.querySelector('#button-guardarcambios');
         ingredientesData.forEach(element => {
             div = document.createElement('div');
             div.classList.add('ingrediente');
@@ -17,14 +35,14 @@ async function getIngredientsinventory () {
                     <label for="ingrediente-${element.id_ingrediente}">${element.nombre}</label>
                     <button>X</button>
             `;
-            document.querySelector('#contenedor-ingredientes').insertBefore(div, botonguardar);
+            document.querySelector('#contenedor-ingredientes').appendChild(div);
             gottenIds.add(element.id_ingrediente);
         })
     } catch (error) {
         console.log(error);
     }
     try {
-        const ingredientes = await fetch(`${API_URL}/ingredients`, {
+        const ingredientes = await fetch(`${API_URL}/ingredientes`, {
             method: 'GET',
             headers: {
                 'Authorization': `Bearer ${token}`
@@ -34,14 +52,19 @@ async function getIngredientsinventory () {
             if(!gottenIds.has(element.id_ingrediente)){
                 div = document.createElement('div');
                 div.classList.add('ingrediente');
+                div.draggable = true;
+                div.setAttribute('id',`${element.id_ingrediente}`)
+                div.addEventListener('dragstart', (event) => drag(event));
                 div.innerHTML = `
                         <input type="number" id="ingrediente-${element.id_ingrediente}" name="ingrediente-${element.id_ingrediente}" placeholder="${element.simbolo}">
                         <label for="ingrediente-${element.id_ingrediente}">${element.nombre}</label>
                         <button>X</button>
                 `;
-                document.querySelector('#contenedor-ingredientes').insertBefore(div, botonguardar);
+                document.querySelector('#contenedor-agregar').appendChild(div);
             }
         })
+    } catch (error) {
+        console.log(error);
     }
 
 }
