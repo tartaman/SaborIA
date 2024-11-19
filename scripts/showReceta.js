3a66c7fdf460b1ba8fe040231a4f575810093e57
Ventthos <ferra04@live.com.mx>
2024-11-19 14:51:15 -0600

    Mostrar recetas

--- scripts/showReceta.js
+++ scripts/showReceta.js
@@ -14,11 +14,49 @@ then(response => response.json()).
 then(data => {
     console.log(data)
     document.querySelector('#title').innerHTML = data[0].titulo
-    document.querySelector('#pasos').innerHTML = data[0].pasos.replace(/\n/g, '<br>')
+
+    const pasos = data[0].pasos.split('\n')  
+    const ol = document.createElement("ul");
+    pasos.forEach(paso => {
+        const li = document.createElement("li");
+        li.innerHTML = paso;
+        ol.appendChild(li);
+    });
+    
+
+    });
+
     document.querySelector('#image').src = `${API_URL}/uploads/${data[0].codigo_imagen}`
-    document.querySelector('#product-info').innerHTML = `
-                                                        <h3>Por: ${data[0].creador}</h3>
-                                                        <h3>Dificultad: ${data[0].dificultad}</h3>
-                                                        <h3>Tiempo: ${data[0].tiempo_preparacion} minutos</h3>
-                                                        `
+    const details = document.querySelectorAll('.recipe-detail');
+    
+    const textos = [`Tiempo de preparación: ${data[0].tiempo_preparacion} minutos`, `Nivel: ${data[0].dificultad}`,
+     `Por: ${data[0].creador}`]
+
+    for (let i = 0; i < details.length; i++) {
+        const detail = document.createElement('p');
+        detail.innerHTML = textos[i];
+        details[i].appendChild(detail);
+    }
+
+    const divIngredientes = document.querySelector('#ingredients div')
+    
+    data[0].ingredientes.forEach(ingrediente => {
+        console.log(ingrediente)
+        const ingredienteDiv = document.createElement('div');
+        ingredienteDiv.classList.add('ingredient-div');
+        ingredienteDiv.innerHTML = `
+            <div>
+                <img src="${ingrediente.codigo_imagen}">
+            <p>${ingrediente.nombre}</p>
+            </div>
+            <div>
+                <p>${ingrediente.cantidad}</p>
+                <b>${ingrediente.simbolo_ingrediente}</b>
+            </div>
+        `;
+        divIngredientes.appendChild(ingredienteDiv);
+    });
+    
+
 });
+
